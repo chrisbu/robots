fc177ea56bc1627eb674b5815686e2361994f635
Chris Buckett <chrisbuckett@gmail.com>
2013-05-22 08:46:40 +0100

    Checkpoint

---
+++ lib/src/robot/commands.dart
@@ -0,0 +1,43 @@
+part of robot;
+
+/// Wrapper to contain the result of the command, which may modify either
+/// the position, direction, or both (or neither).
+class CommandResult {
+  Point position;
+  Direction facing;
+  CommandResult(this.position, this.facing);
+}
+
+/// Interface to define the command.  [execute] the command to
+/// retrieve a command result, which may by applied to the robot.
+abstract class Command {
+  String commandChar;
+  CommandResult execute(Robot robot);
+}
+
+/// Maintins the robots position, but turns the robot to the left
+class TurnLeftCommand implements Command {
+  final commandChar = "L";
+  CommandResult execute(Robot robot) {
+    if (robot == null) throw new ArgumentError("robot");
+    return new CommandResult(robot.position, robot.facing.turnLeft());
+  }
+}
+
+/// Maintins the robots position, but turns the robot to the right
+class TurnRightCommand implements Command {
+  final commandChar = "R";
+  CommandResult execute(Robot robot) {
+    if (robot == null) throw new ArgumentError("robot");
+    return new CommandResult(robot.position, robot.facing.turnRight());
+  }
+}
+
+/// Maintains the robots facing, but moves the robot by 1 grid square
+class MoveForwardCommand implements Command {
+  final commandChar = "F";
+  CommandResult execute(Robot robot) {
+    if (robot == null) throw new ArgumentError("robot");
+    return new CommandResult(robot.facing.move(robot.position), robot.facing);
+  }
+}
